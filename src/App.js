f75741e9148ee011d43f03d36ae967e072ba864b
Lakshmy Mohan Erakandath <lakshmitalks@gmail.com>
2021-11-11 12:24:59 -0800

    created two states to store the items added and an array to keep track of the items

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
+import React, {useState} from 'react';
 import './App.css';
 import Form from './components/Form.js';
 import TodoList from './components/TodoList.js';
 
 function App() {
+  const[inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
       <header>
         <h1>Laks Todo List</h1>
       </header>
-      <Form />
-      <TodoList />
+      <Form
+       inputText = {inputText}
+       setInputText = {setInputText}
+       setTodos = {setTodos}
+       todos = {todos} />
+      <TodoList todos = {todos} />
     </div>
   );
 }
