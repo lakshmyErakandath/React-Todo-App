9ef770a1221989346a5e392e06d392c682e5fff4
Lakshmy Mohan Erakandath <lakshmitalks@gmail.com>
2021-11-11 12:25:55 -0800

    mapped through the todos and displayed each item in the list

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,15 +3,24 @@ import { AiOutlineCheck } from 'react-icons/ai';
 import { BiTrashAlt } from 'react-icons/bi';
 
 
-const TodoList = () => {
+const TodoList = ({ todos }) => {
+  console.log({ todos });
   return (
-    <div className = "listItemContainer">
-      <div className = "listItem">
-      <AiOutlineCheck className = "completed"/>
-      <BiTrashAlt className = "trash" />
-      </div>
-    </div>
+    <div>
+      {todos.map(todo => (
+        <div className="listItemContainer">
+          <div className="listItem">
+            <div>{todo.text}</div>
+            <div>
+            <AiOutlineCheck className="completed" />
+            <BiTrashAlt className="trash" />
+            </div>
+          </div>
+         </div>
+  ))}
+  </div>
   )
 }
+
 
 export default TodoList;
